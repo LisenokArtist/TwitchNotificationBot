b9008a8f79341a8ab25e75d34f95638ee3972ba6
LisenokArtist <lisenokartist@gmail.com>
2025-03-16 22:28:52 +0300

    Убраны критические данные из точки входа

--- TwitchNotificationBot/app.js
+++ TwitchNotificationBot/app.js
@@ -1,9 +1,6 @@
 const TwitchNotificationBot = require("./TwitchNotificationBot");
-const { AppConfig, DiscordConfig } = require("./Core/AppConfig");
-const app = new TwitchNotificationBot(
-    new AppConfig(
-        new DiscordConfig(
-            'MTMzOTI5Mjg5NTMzMjc5NDQ5NA.GOW77y.ok6rrRhYNsx-_aeXNQp3Vr1EDXVHw-WqGE8_Ic',
-            '1339292895332794494',
-            '1203097199945195590')));
+const { AppConfig } = require("./Core/AppConfig");
+const appConfig = new AppConfig();
+appConfig.load();
+const app = new TwitchNotificationBot(appConfig);
 app.Start();
