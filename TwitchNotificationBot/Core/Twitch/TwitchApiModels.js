50fd8139acc1bff6e72d75fadc2b3c7ff45b9839
LisenokArtist <lisenokartist@gmail.com>
2025-03-19 18:59:05 +0300

    Добавлено описание данных twitch api.

--- TwitchNotificationBot/Core/Twitch/TwitchApiModels.js
+++ TwitchNotificationBot/Core/Twitch/TwitchApiModels.js
@@ -1,4 +1,4 @@
-const TokenResponse = function _TokenResponse() {
+﻿const TokenResponse = function TokenResponse() {
     /** @type {String} */
     this.access_token;
     /** @type {String} */
@@ -11,4 +11,54 @@ const TokenResponse = function _TokenResponse() {
     this.token_type;
 }
 
-module.exports = { TokenResponse }
+const StreamsResponse = function StreamsResponse() {
+    /** @type {StreamsResponse[]}*/
+    this.data;
+    /** @type {PaginationResponse}*/
+    this.pagination;
+}
+
+const StreamResponse = function StreamResponse() {
+    /** @type {Number}*/
+    this.id;
+    /** @type {Number}*/
+    this.user_id;
+    /** @type {String}*/
+    this.user_login;
+    /** @type {String}*/
+    this.user_name;
+    /** @type {Number}*/
+    this.game_id;
+    /** @type {String}*/
+    this.game_name;
+    /** @type {String}*/
+    this.type;
+    /** @type {String}*/
+    this.title;
+    /** @type {String[]}*/
+    this.tags;
+    /** @type {Number}*/
+    this.viewer_count;
+    /** @type {String}*/
+    this.started_at;
+    /** @type {String}*/
+    this.language;
+    /** @type {String}*/
+    this.thumbnail_url;
+    /** @type {Number[]}*/
+    this.tag_ids;
+    /** @type {Boolean}*/
+    this.is_mature;
+}
+
+const PaginationResponse = function PaginationResponse() {
+    /** @type {String}*/
+    this.cursor;
+}
+
+module.exports = {
+    TokenResponse,
+    StreamsResponse,
+    StreamResponse,
+    PaginationResponse
+}
