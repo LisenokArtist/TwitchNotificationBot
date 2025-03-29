358269a502cd5adc5b852e181c1ae183fb761665
LisenokArtist <lisenokartist@gmail.com>
2025-03-29 03:54:16 +0300

    Описание свойств

--- TwitchNotificationBot/Core/Collections/KeyValuePair.js
+++ TwitchNotificationBot/Core/Collections/KeyValuePair.js
@@ -1,6 +1,8 @@
 const KeyValuePair = class KeyValuePair {
     constructor(key, value) {
+        /** @type {object}*/
         this.key = key;
+        /** @type {object}*/
         this.value = value;
     }
 }
